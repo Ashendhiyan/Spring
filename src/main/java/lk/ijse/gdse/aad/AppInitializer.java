a18fb5ac381af6b0c00c7319527eaf56e01ed9f0
Ashendhiyan <ashen13516@gmail.com>
2023-10-09 20:22:28 -0800

    Register ShutDown hook method introduced

--- src/main/java/lk/ijse/gdse/aad/AppInitializer.java
+++ src/main/java/lk/ijse/gdse/aad/AppInitializer.java
@@ -1,6 +1,8 @@
 package lk.ijse.gdse.aad;
 
+import lk.ijse.gdse.aad.bean.BeanTwo;
 import lk.ijse.gdse.aad.bean.SpringBean;
+import lk.ijse.gdse.aad.bean.SpringBeanThree;
 import lk.ijse.gdse.aad.config.AppConfig;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -8,13 +10,37 @@ public class AppInitializer {
     public static void main(String[] args){
 
         AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
+
+        //  Run something before JVM shutdown   (Hooking process)
+      /*  Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("JVM is about to shut down..");
+                ctx.close();
+            }
+        }));
+
+*/
+
+        ctx.registerShutdownHook();
+
         ctx.register(AppConfig.class);
         ctx.refresh();
 
+
+        System.out.println("---------------------------------------------------");
+
         SpringBean bean = ctx.getBean(SpringBean.class);
-        bean.testBean();
         System.out.println(bean);
 
-        ctx.close();
+        BeanTwo bean1 = ctx.getBean(BeanTwo.class);
+        System.out.println(bean1);
+
+        SpringBeanThree bean2 = ctx.getBean(SpringBeanThree.class);
+        System.out.println(bean2);
+
+//        ctx.close();
+
+
     }
 }
---
+++ src/main/java/lk/ijse/gdse/aad/bean/SpringBeanThree.java
@@ -0,0 +1,10 @@
+package lk.ijse.gdse.aad.bean;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class SpringBeanThree {
+    public SpringBeanThree(){
+        System.out.println("Object Created from SpringBeanThree..");
+    }
+}
