17ed2d807204936b56f09ae21221ca3b3e5eceb1
Ashendhiyan <ashen13516@gmail.com>
2023-10-11 22:14:19 -0800

    use instant properties

--- _06 - RuntimeValueInnjection/src/main/java/bean/SpringBeanOne.java
+++ _06 - RuntimeValueInnjection/src/main/java/bean/SpringBeanOne.java
@@ -4,9 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-@Component
+//@Component
 public class SpringBeanOne {
-    @Autowired(required = false)
+    @Autowired(required = false)    // greediest  constructor (highest parameter count)
     public SpringBeanOne(@Value("C001") String id, @Value("10") int age, @Value("true") boolean b) {
         System.out.println("Spring Bean one");
         System.out.println(id);
