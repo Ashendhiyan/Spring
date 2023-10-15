6ee4434de81608240f7ba44beeab919a117dee3c
Ashendhiyan <ashen13516@gmail.com>
2023-10-15 14:14:18 -0800

    Introduced maven to the project

---
+++ _10 - SpringWithMaven/src/main/java/lk/ijse/controller/CustomerController.java
@@ -0,0 +1,15 @@
+package lk.ijse.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("customer")
+public class CustomerController {
+
+    @GetMapping
+    public String getAllCustomers(){
+        return "Hello maven..";
+    }
+}
