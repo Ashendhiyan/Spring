871c5654f9054ae4dbe8376532107056a483e66b
Ashendhiyan <ashen13516@gmail.com>
2023-10-15 09:59:08 -0800

    Spring Form Data

---
+++ _09 - RestControllers/src/main/java/lk/ijse/spring/controller/FormDataController.java
@@ -0,0 +1,22 @@
+package lk.ijse.spring.controller;
+
+
+import lk.ijse.spring.dto.CustomerDTO;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("form")
+public class FormDataController {
+
+    @PostMapping("test")
+    public String testForm1(String id,String name){
+        return "Request received.."+id+" "+name;
+    }
+    @PostMapping
+    public String customer(@ModelAttribute CustomerDTO dto){
+        return dto.toString();
+    }
+}
