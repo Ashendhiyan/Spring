fc738f8d5d339f5674d211ef4709c24115042b8e
Ashendhiyan <ashen13516@gmail.com>
2023-10-14 20:50:33 -0800

    path variables

---
+++ _09 - RestControllers/src/main/java/lk/ijse/spring/controller/PathVariableController.java
@@ -0,0 +1,30 @@
+package lk.ijse.spring.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("variable")
+public class PathVariableController {
+
+    //http://localhost:8080/restcontroller/variable/I001
+
+    // (variable)  path segments
+    // (I001) path segment
+
+    //In spring we can retrieve value of path segments
+    // to do that we hava to crate path variables -> {I001}
+
+    @GetMapping(path = "{id}")
+    public String test(@PathVariable("id") String itemCode){
+        return itemCode;
+    }
+
+    @GetMapping(path = "{id}/{name}")
+    public String test2(@PathVariable String id,@PathVariable String name){
+        return id+" "+name;
+    }
+
+}
