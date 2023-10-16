77382209f3630ab8d2fc0eec1f58ca341a4b9b42
Ashendhiyan <ashen13516@gmail.com>
2023-10-16 15:51:22 -0800

    content updated with produces,consumes & headers

--- _10 - SpringWithMaven/src/main/java/lk/ijse/controller/CustomerController.java
+++ _10 - SpringWithMaven/src/main/java/lk/ijse/controller/CustomerController.java
@@ -10,13 +10,34 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("customer")
 public class CustomerController {
 
-    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    //   We can narrow down requests using Headers also
+
+
+    //  produce -> response type json
+    // for frontEnd validation = Accept -> application/json     ---> only get as response (application/json type)
+ /*   @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public CustomerDTO getAllCustomers(){
         return new CustomerDTO("C001","Ashen","Hatton",5000.00);
     }
 
+    //  consume -> application/json  (request body should include Content_type=application/json header )
     @GetMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     public CustomerDTO searchCustomers(){
         return new CustomerDTO("C002","bawantha","Hatton",5000.00);
+    }*/
+
+
+    @GetMapping(consumes = "text/html" ,  produces = "application/json")
+    public CustomerDTO testMethodOne(){
+        return new CustomerDTO("C001","TEXT","Kurunegala",3000);
+    }
+    @GetMapping(consumes = "application/json" ,  produces = "application/json")
+    public CustomerDTO testMethodTwo(){
+        return new CustomerDTO("C002","JSON","Kurunegala",3000);
+    }
+    @GetMapping(headers = {"Content-Type=text/css","Accept=application/json"})
+    public CustomerDTO testMethodThree(){
+        return new CustomerDTO("C003","Headers","Kurunegala",3000);
     }
 }
+
