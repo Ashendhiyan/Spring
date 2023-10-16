04b596ea92130a4fb9bd4e3e8a2bf462d820f9da
Ashendhiyan <ashen13516@gmail.com>
2023-10-16 03:42:43 -0800

    content updated

--- _10 - SpringWithMaven/src/main/java/lk/ijse/controller/CustomerController.java
+++ _10 - SpringWithMaven/src/main/java/lk/ijse/controller/CustomerController.java
@@ -1,5 +1,7 @@
 package lk.ijse.controller;
 
+import lk.ijse.dto.CustomerDTO;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("customer")
 public class CustomerController {
 
-    @GetMapping
-    public String getAllCustomers(){
-        return "Hello maven..";
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public CustomerDTO getAllCustomers(){
+        return new CustomerDTO("C001","Ashen","Hatton",5000.00);
     }
 }
